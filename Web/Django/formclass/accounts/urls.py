da1a45507c3c6db42401fdfa9186a5113f6741f6
NayeonKim127 <rkgh0411@gmail.com>
2020-06-24 09:33:28 +0900

    TIL

---
+++ Web/Django/formclass/accounts/urls.py
@@ -0,0 +1,14 @@
+# accounts > urls.py
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'), # User [C]reate
+    path('login/', views.login, name='login'), # Session [C]reate
+    path('logout/', views.logout, name='logout'), # Session [D]elete
+    path('delete/', views.delete, name='delete'), # User [D]elete
+    path('edit/', views.edit, name='edit'), # User [U]pdate
+    path('password/', views.password, name='password'), # (Password) [U]pdate
+]
